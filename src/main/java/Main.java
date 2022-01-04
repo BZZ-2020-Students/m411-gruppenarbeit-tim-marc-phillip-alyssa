df71f4fa3e187e049b1abc93561130163d915549
Tim Irmler <zwazel@hotmail.com>
2022-01-04 09:46:35 +0100

    quicksort works

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
+import sortAlgorithms.Quicksort;
+
 public class Main {
     public static void main(String[] args) {
-        
+        int[] array = {4,1,2,8,6,5};
+        Quicksort quicksort = new Quicksort();
+        quicksort.sort(array);
     }
 }
--- src/main/java/sortAlgorithms/Quicksort.java
+++ src/main/java/sortAlgorithms/Quicksort.java
@@ -1,32 +1,33 @@
 package sortAlgorithms;
 
+import util.Measurement;
+
 import java.util.Arrays;
-import java.util.Date;
-import java.util.Random;
 
-public class Quicksort {
-    private int[] unsortedArray;
-    private final int SIZE =10;
-    private final int RANGE = 10;
+public class Quicksort implements Sorter {
+    @Override
+    public Measurement sort(int[] array) {
+        sort(array, 0, array.length-1);
+        System.out.println("array = " + Arrays.toString(array));
 
-    Quicksort() {
-        Random rand = new Random();
-        unsortedArray = new int[SIZE];
-        for (int i = 0; i < unsortedArray.length; i++) {
-            unsortedArray[i] = rand.nextInt(RANGE);
+        return null;
+    }
+
+    private void swap(int[] array, int a, int b) {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+
+    private void sort(int[] arr, int links, int rechts) {
+        if (links < rechts) {
+            int split = teile(arr, links, rechts);
+            sort(arr, links, split - 1);
+            sort(arr, split + 1, rechts);
         }
     }
 
-    private void sort(int[] arr,int links,int rechts) {
-        if(links < rechts) {
-            int split = teile(arr,links, rechts);
-            sort(arr,links, split - 1);
-            sort(arr,split + 1, rechts);
-        }
-
-    }
-
-    private int teile(int[] arr,int links,int rechts){
+    private int teile(int[] arr, int links, int rechts) {
         int i = links;
         // Starte mit j links vom Pivotelement
         int j = rechts - 1;
@@ -34,37 +35,25 @@ public class Quicksort {
 
         while (i < j) { // solange i an j nicht vorbeigelaufen ist
             // Suche von links ein Element, welches größer oder gleich dem Pivotelement ist
-            while(i<rechts && arr[i] < pivot) {
-                i=i + 1;
+            while (i < rechts && arr[i] < pivot) {
+                i = i + 1;
             }
 
             // Suche von rechts ein Element, welches kleiner als das Pivotelement ist
-            while (j > links && arr[j] >=pivot) {
-                j=j - 1;
+            while (j > links && arr[j] >= pivot) {
+                j = j - 1;
             }
 
-            if (i <j) {
-                swap(arr,i, j);
+            if (i < j) {
+                swap(arr, i, j);
             }
-
-
         }
-
 
         // Tausche Pivotelement (daten[rechts]) mit neuer endgültiger Position (daten[i])
         // und gib die neue Position des Pivotelements zurück, beende Durchlauf
         if (arr[i] > pivot) {
-            swap(arr,i,rechts);
+            swap(arr, i, rechts);
         }
         return i;
     }
-
-    private static void swap(int[] array, int a, int b) {
-        int temp = array[a];
-        array[a] = array[b];
-        array[b] = temp;
-    }
-
-
-
 }
