73432628a0cf73ff1dca89c78d8b685ae9628065
Tim Irmler <zwazel@hotmail.com>
2022-01-04 09:46:43 +0100

    format

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import sortAlgorithms.Quicksort;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = {4,1,2,8,6,5};
+        int[] array = {4, 1, 2, 8, 6, 5};
         Quicksort quicksort = new Quicksort();
         quicksort.sort(array);
     }
