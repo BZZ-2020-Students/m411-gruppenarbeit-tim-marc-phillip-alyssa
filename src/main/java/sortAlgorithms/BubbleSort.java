80c05d8f8f1c7d8aa73834925d0776317bfb8e91
Alyssa <heimlichera@bzz.ch>
2022-01-04 09:28:36 +0100

    BubbleSort Start

---
+++ src/main/java/sortAlgorithms/BubbleSort.java
@@ -0,0 +1,18 @@
+package sortAlgorithms;
+
+import util.Measurement;
+/**
+ * Bubble Sort Algorithmus
+ *
+ * @author Alyssa Heimlicher
+ * @since 2022-01-04
+ * @version 1.0
+ *
+ */
+
+public class BubbleSort implements Sorter{
+    @Override
+    public Measurement sort(int[] array) {
+        return null;
+    }
+}
