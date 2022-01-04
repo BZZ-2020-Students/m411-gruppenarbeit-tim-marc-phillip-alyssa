6fba65ba1549535d43da7ff2c5e5e61ab91dba86
Tim Irmler <zwazel@hotmail.com>
2022-01-04 11:05:29 +0100

    TREE SORT DONE

--- src/main/java/sortAlgorithms/TreeSort.java
+++ src/main/java/sortAlgorithms/TreeSort.java
@@ -2,25 +2,18 @@ package sortAlgorithms;
 
 import util.Measurement;
 
-import java.util.Arrays;
-
 public class TreeSort implements Sorter {
-    // Root of BST
     Node root;
+    int index = 0;
 
-    // Constructor
     public TreeSort() {
         root = null;
     }
 
-    // This method mainly
-    // calls insertRec()
     void insert(int key) {
         root = insertRec(root, key);
     }
 
-    /* A recursive function to
-    insert a new key in BST */
     private Node insertRec(Node root, int key) {
         /* If the tree is empty,
         return a new node */
@@ -31,8 +24,6 @@ public class TreeSort implements Sorter {
             return root;
         }
 
-        /* Otherwise, recur
-        down the tree */
         measure.increaseComparisons();
         if (key < root.key) {
             root.left = insertRec(root.left, key);
@@ -40,23 +31,21 @@ public class TreeSort implements Sorter {
             root.right = insertRec(root.right, key);
         }
 
-        /* return the root */
         return root;
     }
 
-    // A function to do
-    // inorder traversal of BST
-    private void inorderRec(Node root, int[] sortedArray, int index) {
+    private void putInOrder(Node root, int[] sortedArray) {
         measure.increaseIterations();
         measure.increaseComparisons();
         if (root != null) {
-            inorderRec(root.left, sortedArray, index);
+            putInOrder(root.left, sortedArray);
             sortedArray[index++] = root.key;
-            inorderRec(root.right, sortedArray, index);
+//            System.out.print(root.key + " ");
+            putInOrder(root.right, sortedArray);
         }
     }
 
-    void treeins(int[] arr) {
+    void insertEverything(int[] arr) {
         for (int j : arr) {
             insert(j);
         }
@@ -65,16 +54,12 @@ public class TreeSort implements Sorter {
     @Override
     public Measurement sort(int[] array) {
         long time = System.currentTimeMillis();
-        treeins(array);
-        inorderRec(this.root, new int[array.length], 0);
+        insertEverything(array);
+        putInOrder(this.root, array);
         measure.setTime(System.currentTimeMillis() - time);
-        System.out.println("array = " + Arrays.toString(array));
         return measure;
     }
 
-    // Class containing left and
-    // right child of current
-    // node and key value
     private static class Node {
         int key;
         Node left, right;
