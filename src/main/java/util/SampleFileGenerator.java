7914953be2604a4b542d7c350235828121eaf546
Marc-Andri Fuchs <marconymous@outlook.com>
2022-01-11 14:29:30 +0100

    save generated data file in correct folder because lazy

--- src/main/java/util/SampleFileGenerator.java
+++ src/main/java/util/SampleFileGenerator.java
@@ -7,7 +7,7 @@ public class SampleFileGenerator {
     private static final int size = 100000;
 
     public static void main(String[] args) throws IOException {
-        FileWriter fw = new FileWriter(size + "Digits.dat");
+        FileWriter fw = new FileWriter("src/main/resources/inputfiles/" + size + "Digits.dat");
 
         for (int i = 0; i < size; i++) {
             var rand = (int) (Math.random() * size);
